6841ac787f51989c7d14ab9dfa5b1f8b4bb70376
dujesaric <dsaric02@fesb.hr>
2020-12-02 23:32:56 +0100

    Implemented event repository and method for fetching from dictionary by event name. Finished event details task.

--- EventAttendanceApp/EventAttendanceApp/Program.cs
+++ EventAttendanceApp/EventAttendanceApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using EventAttendanceApp.Factories;
 using EventAttendanceApp.Models;
+using EventAttendanceApp.Repositories;
 
 namespace EventAttendanceApp
 {
@@ -36,7 +37,7 @@ namespace EventAttendanceApp
                     case 5:
                         break;
                     case 6:
-                        DisplayEventsDetailsSubmenu();
+                        HandleEventReviewDisplay(events);
                         break;
                 }
 
@@ -56,8 +57,24 @@ namespace EventAttendanceApp
 
             for (int i = 0; i < events.Count; i++)
             {
+                KeyValuePair<Event, List<Attendee>> eventsAndAttendees = eventsEnumerator.Current;
+                Console.WriteLine(eventsAndAttendees.Key.ToString());
+
+                eventsEnumerator.MoveNext();
+            }
+        }
+        
+        private static void DisplayAllEventsNames(Dictionary<Event, List<Attendee>> events)
+        {
+            Console.WriteLine("Svi nazivi evenata:");
+
+            var eventsEnumerator = events.GetEnumerator();
+            eventsEnumerator.MoveNext();
+
+            for (int i = 0; i < events.Count; i++)
+            {
                 KeyValuePair<Event, List<Attendee>> eventAndAttendees = eventsEnumerator.Current;
-                Console.WriteLine(eventAndAttendees.Key.ToString());
+                Console.WriteLine(eventAndAttendees.Key.Name);
 
                 eventsEnumerator.MoveNext();
             }
@@ -68,6 +85,50 @@ namespace EventAttendanceApp
             var newEvent = EventFactory.CreateNew();
 
             events.Add(newEvent, null);
+        }
+
+        private static void HandleEventReviewDisplay(Dictionary<Event, List<Attendee>> events)
+        {
+            int userInput;
+
+            do
+            {
+                userInput = FetchUsersInputFromEventDetailsSubmenu();
+                Console.Clear();
+
+                switch (userInput)
+                {
+                    case 1:
+                        DisplayEventDetails(events);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (userInput != 4);
+        }
+        private static void DisplayEventDetails(Dictionary<Event, List<Attendee>> events)
+        {
+            Console.WriteLine("Ispis svih evenata po imenima:");
+            DisplayAllEventsNames(events);
+
+            Console.WriteLine("Molimo vas unesite ime eventa kojeg želite detaljnije pregledati:");
+            var queryName = Console.ReadLine();
+
+            var foundEvent = EventRepository.GetByName(events, queryName);
+
+            if (foundEvent is Event)
+            {
+                foundEvent.ToString();
+            }
+            else
+            {
+                Console.WriteLine($"Event pod imenom {queryName} nije pronađen.");
+            }
+            
         }
 
         private static int FetchUsersInputFromMenu()
---
+++ EventAttendanceApp/EventAttendanceApp/Repositories/EventRepository.cs
@@ -0,0 +1,29 @@
+﻿using EventAttendanceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAttendanceApp.Repositories
+{
+    public static class EventRepository
+    {
+        public static Event? GetByName(Dictionary<Event, List<Attendee>> eventsAndAttendees, string name)
+        { 
+            IEnumerator<Event> eventEnumerator = eventsAndAttendees.Keys.GetEnumerator();
+            eventEnumerator.MoveNext();
+
+            for (int i = 0; i < eventsAndAttendees.Count; i++)
+            {
+                var currentEvent = eventEnumerator.Current;
+                if (currentEvent.Name.Equals(name))
+                {
+                    return currentEvent;
+                }
+
+                eventEnumerator.MoveNext();
+            }
+
+            return null;
+        }
+    }
+}
